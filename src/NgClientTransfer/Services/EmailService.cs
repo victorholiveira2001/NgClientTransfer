58c3b18f4322a9a5ac79a26b7634789374054f45
Victor Hugo Nogueira Oliveira <victor@casa.local>
2024-08-04 02:41:01 -0300

    Iniciando configuração de serviço de email

---
+++ src/NgClientTransfer/Services/EmailService.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+
+namespace NgClientTransfer.Services
+{
+    internal class EmailService : IEmailService
+    {
+        private string Email { get; set; }
+        private string Senha { get; set; }
+        private string Host { get; set; }
+        private int Port { get; set; }
+
+
+        private readonly Configuration _configuration;
+
+        public EmailService(Configuration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void DisparaEmail(string conteudo, List<string> emails)
+        {
+            _configuration.GetSectionGroup("EmailSettings");
+
+             
+
+            var smtpClient = new SmtpClient(Host, Port);
+            smtpClient.Credentials = new NetworkCredential(Email, Senha);
+        }
+    }
+}
--- src/NgClientTransfer/Services/ManipulaArquivosService.cs
+++ src/NgClientTransfer/Services/ManipulaArquivosService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Globalization;
+using System.Linq;
+using System.Numerics;
 using NgClientTransfer.Services;
 
 namespace NgClientTransfer.Services
@@ -6,18 +8,24 @@ namespace NgClientTransfer.Services
     internal class ManipulaArquivosService : IManipulaArquivosService
     {
         private readonly ProcedureService _procedureService;
-
-        public ManipulaArquivosService(ProcedureService procedureService)
-        {
-               _procedureService = procedureService;
-        }
+        private readonly ServiceManagerService _serviceManager;
+        private string Edi = Environment.GetEnvironmentVariable("EDI");
+        private string NgClientOut = Environment.GetEnvironmentVariable("NgClientOut");
+        private string NgClientSent = Environment.GetEnvironmentVariable("NgClientSent");
         private DateTime Data { get; set; }
+
+        public ManipulaArquivosService(ProcedureService procedureService, ServiceManagerService serviceManager)
+        {
+            _procedureService = procedureService;
+            _serviceManager = serviceManager;
+        }
+
         public void VerificaArquivos()
         {
             while (true)
             {
                 Data = DateTime.ParseExact(DateTime.Now.ToString(), "yyyyMMdd", CultureInfo.InvariantCulture);
-                string[] arquivos = Directory.GetFiles($@"\\172.16.1.1\Consinco\EDI\").Select(Path.GetFileName).ToArray();
+                string[] arquivos = Directory.GetFiles(Edi).Select(Path.GetFileName).ToArray();
                 MovimentaArquivos(arquivos);
 
                 if (DateTime.Now.ToString("HH:mm") == "05:00")
@@ -26,8 +34,7 @@ namespace NgClientTransfer.Services
                 }
                 else if (DateTime.Now.ToString("HH:mm") == "19:00")
                 {
-                    string[] àrquivos = Directory.GetFiles(@"C:\NeoGridClient\documents\sent\").Select(Path.GetFileName).ToArray();
-                    ConfereExisteArquivos(arquivos);
+                    ConfereExisteArquivos();
                 }
 
                 Thread.Sleep(60000);
@@ -36,59 +43,58 @@ namespace NgClientTransfer.Services
 
         public void MovimentaArquivos(string[] arquivos)
         {
-            while (true)
+
+            Data = DateTime.ParseExact(DateTime.Now.ToString(), "yyyyMMdd", CultureInfo.InvariantCulture);
+            if (arquivos.Length >= 4)
             {
-                Data = DateTime.ParseExact(DateTime.Now.ToString(), "yyyyMMdd", CultureInfo.InvariantCulture);
-                if (arquivos.Length >= 4)
+                try
                 {
-                    try
+                    foreach (var arquivo in arquivos)
                     {
-                        foreach (var arquivo in arquivos)
+                        //Se menor que 17, devo excluir para não dar erro na comparação de datas atralado ao nome.
+                        if (arquivo.Length < 17) 
                         {
-                            if (arquivo.Length < 17)
-                            {
-                                File.Delete($@"\\172.16.1.1\Consinco\EDI\{arquivo}");
-                            }
-                            else if (arquivo.Substring(9, 8) == Data.ToString() || 
-                                     arquivo.Substring(8, 8) == Data.ToString())
-                            {
-                                File.Move($@"\\172.16.1.1\Consinco\EDI\{arquivo}",
-                                          $@"C:\NeoGridClient\documents\out\{arquivo}");
-                            }
-                            else
-                            {
-                                File.Delete($@"\\172.16.1.1\Consinco\EDI\{arquivo}");
-                            }
+                            File.Delete($@"{Edi}\{arquivo}");
                         }
-
-                        var serviceManager = new ServiceManagerService();
-                        serviceManager.IniciarServico();
-
+                        else if (arquivo.Substring(9, 8) == Data.ToString() || 
+                                 arquivo.Substring(8, 8) == Data.ToString())
+                        {
+                            File.Move($@"{Edi}\{arquivo}",
+                                      $@"{NgClientOut}\{arquivo}");
+                        }
+                        else
+                        {
+                            File.Delete($@"{Edi}\{arquivo}");
+                        }
                     }
-                    catch (Exception ex)
-                    {
-                         // TODO Implementar mensageria.
-                         // TODO Tratamento, se houver.
-                    }
+
+                    _serviceManager.IniciarServico();
+
+                }
+                catch (Exception ex)
+                {
+                        // TODO Implementar mensageria.
+                        // TODO Tratamento, se houver.
                 }
             }
         }
 
 
-        public void ConfereExisteArquivos(string[] arquivos)
+        public void ConfereExisteArquivos()
         {
-            int contador = 0;
             Data = DateTime.ParseExact(DateTime.Now.ToString(), "yyyyMMdd", CultureInfo.InvariantCulture);
 
-            foreach (string arquivo in arquivos)
-            {
-                if (arquivo.Substring(9, 8) == Data.ToString() || 
-                    arquivo.Substring(8, 8) == Data.ToString())
+
+            var arquivos = Directory.GetFiles(NgClientSent)
+                .Where(arquivo =>
                 {
-                    contador++;
-                }
-            }
-            if (contador < 4)
+                    var nomearquivo = Path.GetFileName(arquivo);
+                    return nomearquivo.Substring(9, 8) == Data.ToString() || nomearquivo.Substring(8, 8) == Data.ToString();
+                })
+                .Select(Path.GetFileName)
+                .ToArray();
+
+            if (arquivos.Length < 4)
             {
                 _procedureService.ExecutarProcedure();
             }
